8e0d754d2729d1a003a09fa2e2bf8bc4324bbe29
DraeRamsey <andrea.jos.ramsey@gmail.com>
2020-08-04 16:45:25 -0700

    Add render game function to module pattern

--- js/script.js
+++ js/script.js
@@ -1,41 +1,29 @@
 'use strict';
-//step 1: function that renders array to the html
-//*** create a space in the html for the array to render
-//*** link that space to the JavaScript
-//**** make a simple array and function outside of the objects and have that function link up the HTML
-//** make the square html grid
-//*** next we will ficure out how to encapsulate that function inside the gameboard module and render it from there
 
 const game_container = document.getElementById('game-container');
 var grid = "";
 
-// testing
-var gameboard = ["o","x","x","o","x","x","o","o","x"];
 
-function renderGameBoard()
-{
-  for (var i = 0; i < gameboard.length; i++)
-  {
-   console.log( gameboard[i]);
-   console.log(i);
-   grid =  document.querySelector(`[data-index-number="${i}"]`);
-   grid.textContent = gameboard[i];
+//make module
+//parameters? boardplacement/ player type
+var GameBoard = (function() {
+  var gameboard = ["-","-","-","-","-","-","-","-","-"];
+  return {
+    gameboard,
+    renderGameBoard: function() {
+      for (var i = 0; i < gameboard.length; i++)
+      {
+       grid =  document.querySelector(`[data-index-number="${i}"]`);
+       grid.textContent = gameboard[i];
 
+      }
+    }
+  };
+})();
 
-  }
-};
-renderGameBoard();
+GameBoard.renderGameBoard();
+console.log(GameBoard.gameboard);
 
-//testing
-
-//make moduleæ'""'
-// const GameBoard = (function()
-// {
-//   // Set up HTML and write a JavaScript function that will render the contents of the gameboard array to the webpage (for now just manually fill in the array with "X"s and "O"s
-//    var gameboard = ["o","x","x","o","x","x"];
-//
-//   return {gameboard};
-// })();
 
 // push the info to the array
 
@@ -48,8 +36,10 @@ const GameController = () =>
 }
 
 //factory function
+//player should be marked with x or o (playertype)
 const Player = () =>
 {
+
 
   //return {};
 }
