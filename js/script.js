ed154ed1c172fb2a891da65f4c572e0332cbfdb6
DraeRamsey <andrea.jos.ramsey@gmail.com>
2020-08-05 16:08:52 -0700

    Add game controller functions
    marks where the player clicked with either an X or an O based on whose turn it is

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
 'use strict';
 
+
 const game_container = document.getElementById('game-container');
 var grid = "";
 const game_squares = document.querySelectorAll('[data-index-number]');
+
 
 
 //make module
 //parameters? boardplacement/ player type
 var GameBoard = (function() {
   var gameboard = ["-","-","-","-","-","-","-","-","-"];
+
+   const renderGameBoard = function() {
+    for (var i = 0; i < gameboard.length; i++)
+    {
+     grid =  document.querySelector(`[data-index-number="${i}"]`);
+     grid.textContent = gameboard[i];
+
+    }
+
+  }
+
   return {
     gameboard,
-    renderGameBoard: function() {
-      for (var i = 0; i < gameboard.length; i++)
-      {
-       grid =  document.querySelector(`[data-index-number="${i}"]`);
-       grid.textContent = gameboard[i];
-
-      }
-    }
+    renderGameBoard
   };
 })();
 
@@ -26,10 +32,12 @@ var GameBoard = (function() {
 GameBoard.renderGameBoard();
 console.log(GameBoard.gameboard);
 
+
 const Player = (name, type) =>
 {
 const play = () => console.log(name + ' plays ' + type);
 return { name, type, play};
+
 }
 
 const GameController = (() =>
@@ -45,8 +53,14 @@ const GameController = (() =>
   for( var i = 0; i < game_squares.length;i++)
   {
     game_squares[i].addEventListener('click', function(){
-      console.log(this);
-      console.log(player_turn.name);
+
+      var this_square = this.getAttribute('data-index-number');
+      var board = GameBoard.gameboard;
+
+      board[this_square] = player_turn.type;
+      console.log(board);
+      GameBoard.renderGameBoard();;
+
       //this.type goes into html (update game board)
       //replace the gameboard array index with this.type
       counter ++;
@@ -62,9 +76,16 @@ const GameController = (() =>
     return player_turn;
   }
 
+  function updateGame()
+  {
+
+  }
+
   const playing = () => console.log(GameBoard.gameboard);
   return{
+
   };
 })();
+
 
 //space
