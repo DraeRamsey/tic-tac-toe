cf77a6c9968f5ee37cf714aefe34132fc673b0fe
DraeRamsey <andrea.jos.ramsey@gmail.com>
2020-08-05 15:17:59 -0700

    Start Controller and Player objects

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
 'use strict';
 
-
-
-// TODO:
-//next start the player factory function
-
-
 const game_container = document.getElementById('game-container');
 var grid = "";
+const game_squares = document.querySelectorAll('[data-index-number]');
 
 
 //make module
-//parameters? board placement/ player type
+//parameters? boardplacement/ player type
 var GameBoard = (function() {
   var gameboard = ["-","-","-","-","-","-","-","-","-"];
   return {
@@ -27,31 +22,49 @@ var GameBoard = (function() {
   };
 })();
 
+
 GameBoard.renderGameBoard();
 console.log(GameBoard.gameboard);
 
-//controls the flow of the game itself
-const GameController = () =>
-{
-
-
-  //return {};
-}
-
-//factory function
 const Player = (name, type) =>
 {
 const play = () => console.log(name + ' plays ' + type);
 return { name, type, play};
-
 }
 
-const player1 = Player("jeff", "X");
-player1.play();
+const GameController = (() =>
+{
+  //input: players put in their names here
+  const player1 = Player("jeff", "X");
+  const player2 = Player("ben", "O");
+  let player_turn = null;
+  let counter = 0 //go up to 9
+  determineTurn();
 
+  //turns squares into node list and adds a click event to each one
+  for( var i = 0; i < game_squares.length;i++)
+  {
+    game_squares[i].addEventListener('click', function(){
+      console.log(this);
+      console.log(player_turn.name);
+      //this.type goes into html (update game board)
+      //replace the gameboard array index with this.type
+      counter ++;
+      determineTurn();
+      // run player turn function
 
+    });
+  }
 
+  function determineTurn()
+  {
+    counter % 2 === 0 ? player_turn = player1 : player_turn = player2
+    return player_turn;
+  }
 
-
+  const playing = () => console.log(GameBoard.gameboard);
+  return{
+  };
+})();
 
 //space
