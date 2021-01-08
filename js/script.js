85998af21fe05a364b87b66d3029650720971568
DraeRamsey <andrea.jos.ramsey@gmail.com>
2021-01-08 12:27:52 -0800

    clean up board interface, stop game when winner is declared

--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
 'use strict';
 
 // TODO:
-// check if there are three in a row/ win clause
-    //store board as 2d array
-    // check the rows columns and diagonals
+
 //check if there is a tie/ limit counter
 // clean up returns
 //clean up interface
 
 //make sure to assign the play function for user exper
-
-
 //const container = document.querySelector('#container');
 
+// create overlay over squares when game is finished
 //testing
-
-
-
 
 
 //testing
@@ -25,11 +19,9 @@ var grid = "";
 
 //create node list of each square  with it's placement number
 const game_squares = document.querySelectorAll('[data-index-number]');
-
-
 var GameBoard = (function()
  {
-  var gameboard = ["0","1","2","3","4","5","6","7","8"];
+  var gameboard = [];
 
    const renderGameBoard = function()
   {
@@ -87,44 +79,52 @@ const GameController = (() =>
   for( var i = 0; i < game_squares.length;i++)
   {
     game_squares[i].addEventListener('click', function(){
-
-      var this_square = this.getAttribute('data-index-number');
-      var board = GameBoard.gameboard;
-
-      //check if mark has already been made to the board
-      if (board[this_square] === player1.type || board[this_square] === player2.type)
+      if(winner){
         return;
-      else
-      {
-        board[this_square] = player_up.type;
-        GameBoard.renderGameBoard();
-
-      //win logic 
-       function checkWinStatus(){
-         winning_combos.forEach((item) =>{
-           if ( GameBoard.gameboard[item[0]] === player_up.type
-             && GameBoard.gameboard[item[1]] === player_up.type
-             && GameBoard.gameboard[item[2]] === player_up.type){
-               console.log(player_up.name + "wins!");
-             }
-
-            //console.log(player_up.type);
-
-
-         })
-       }
-      //check for win
-
-        counter ++;
-
-        //testing
-        checkWinStatus();
-        //testing
-        determineTurn();
-      GameBoard.gameboard
-
-
       }
+
+      else{
+        var this_square = this.getAttribute('data-index-number');
+        var board = GameBoard.gameboard;
+
+        //check if mark has already been made to the board
+        if (board[this_square] === player1.type || board[this_square] === player2.type)
+          return;
+        else
+        {
+          board[this_square] = player_up.type;
+          GameBoard.renderGameBoard();
+
+        //win logic
+         function checkWinStatus(){
+           winning_combos.forEach((item) =>{
+             if ( GameBoard.gameboard[item[0]] === player_up.type
+               && GameBoard.gameboard[item[1]] === player_up.type
+               && GameBoard.gameboard[item[2]] === player_up.type){
+                 console.log(player_up.name + " wins!");
+                 winner = true;
+                 return winner;
+               }
+
+
+
+
+           })
+         }
+        //check for win
+
+          counter ++;
+
+          //testing
+          checkWinStatus();
+          //testing
+          determineTurn();
+        GameBoard.gameboard
+
+
+        }
+      }
+
     });
   }
 
